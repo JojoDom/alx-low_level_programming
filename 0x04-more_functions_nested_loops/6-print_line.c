533217b179b390208683fa17694e860ba20d6cfe
Adwoa Dadson <93986114+JojoDom@users.noreply.github.com>
2023-02-24 00:14:00 +0000

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,20 +2,15 @@
 
 /**
  * print_line - draws a straight line in the terminal
- * @n: number of times the character should be printed
+ * @n: number of times the character _ should be printed
+ * Return: nothing
  */
 void print_line(int n)
 {
-	if (n <= 0)
-	{
-		_putchar('\n');
-	} else
-	{
-		int i;
-		for (i = 1; i <= n; i++)
-		{
-			_putchar('_');
-		}
-		_putchar('\n');
-	}
+	int i;
+
+	for (i = 0; i < n; i++)
+		_putchar('_');
+
+	_putchar('\n');
 }
